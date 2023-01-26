5e5817f482f809629fae1b35a130d0c205eda460
Dan Coman <dancoman@Dans-MacBook-Pro.local>
2023-01-26 14:48:14 +0000

    Rewrite to use one liners instead of lots of lists

--- cipher.py
+++ cipher.py
@@ -1,34 +1,20 @@
 def cipher(string, shift):
-    lst = list(string)
-    ordinal = []
+    code = ""
+
+    for i in range(0, len(string)):
+        letter = string[i]
     
-    for item in lst:
-        ordinal.append(ord(item))
+        if letter == " ":
+            code+=" "
+        elif (letter.isupper()):
+            code += chr((ord(letter) + shift-65) % 26 + 65)
+        else:
+            code += chr((ord(letter) + shift-97) % 26 + 97)
 
-    shifted = []        
-    for num in ordinal:
-        if num == 32:
-            shifted.append(num)
-        else: 
-            shifted.append(num + shift)
-    
-    # num_wrap = []
-    # for i in shifted:
-    #     if 65 <= i <= 90:
-    #         num_wrap.append(((i - 65 + shift) % 26) + 65)
-    #     elif 97 <= i <= 122:
-    #         num_wrap.append(((i - 97 + shift) % 26) + 97)
-    #     else:
-    #         num_wrap.append(i)
-
-    letters_shifted = []
-    for num in shifted:
-        letters_shifted.append(chr(num))
-
-    code = ''.join(letters_shifted)
     print(code)
 
 cipher("Lorem ipsum dolor sit amet", 1)
 cipher("Lorem ipsum dolor sit amet", 10)
 cipher("Lorem ipsum dolor sit amet", 15)
+
 #def decode(string, shift):
