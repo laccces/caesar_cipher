23eb7cbd3d65b6b9bdb69885ea46b25dc6570c21
Dan Coman <dancoman@Dans-MacBook-Pro.local>
2023-01-26 14:53:01 +0000

    Added decode function.

--- cipher.py
+++ cipher.py
@@ -17,4 +17,21 @@ cipher("Lorem ipsum dolor sit amet", 1)
 cipher("Lorem ipsum dolor sit amet", 10)
 cipher("Lorem ipsum dolor sit amet", 15)
 
-#def decode(string, shift):
+def decode(string, shift):
+    decoded = ""
+
+    for i in range(0, len(string)):
+        letter = string[i]
+    
+        if letter == " ":
+            decoded+=" "
+        elif (letter.isupper()):
+            decoded += chr((ord(letter) - shift-65) % 26 + 65)
+        else:
+            decoded += chr((ord(letter) - shift-97) % 26 + 97)
+    
+    print(decoded)
+
+decode("Mpsfn jqtvn epmps tju bnfu", 1)
+decode("Vybow szcew nyvyb csd kwod", 10)
+decode("Adgtb xehjb sdadg hxi pbti", 15)
